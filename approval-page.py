2f17dc840b249616451316f0778ad786fed59c25
Marcos Paulo <marcospaulosou12@gmail.com>
2025-02-27 14:13:24 -0300

    Correções no código

--- approval-page.py
+++ approval-page.py
@@ -154,7 +154,7 @@ if not hu_data.empty:
         }
         </style>
         <div class="footer">
-            <a href="https://ibb.co/VWYsWHtT"><img src="https://i.ibb.co/VWYsWHtT/rodap-pagina-de-aprova-o-1.png" alt="Grupo Somapay" border="0"></a>
+            <a href="https://ibb.co/VWYsWHtT"><img alt="Grupo Somapay" border="0"></a><br /><a target='_blank'/>
         </div>
         """,
         unsafe_allow_html=True
