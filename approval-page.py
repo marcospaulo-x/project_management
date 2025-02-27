944e521dde995186ab5fdd4c1ff14edd69cda6dd
Marcos Paulo <marcospaulosou12@gmail.com>
2025-02-27 14:53:41 -0300

    Correções no código

--- approval-page.py
+++ approval-page.py
@@ -132,15 +132,13 @@ if not hu_data.empty:
             <style>
             .footer {{
                 text-align: center;
-                margin-top: 40px;
+                margin-top: 100px;
             }}
             .footer img {{
-                width: 300px; /* Ajuste o tamanho da imagem */
+                width: 400px; /* Ajuste o tamanho da imagem */
                 display: block;
                 margin-left: auto;
-                margin-right: auto;
-                border: 2px solid #ddd; /* Borda cinza */
-                border-radius: 10px; /* Bordas arredondadas */
+                margin-right: auto;                              
                 box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); /* Sombra suave */
             }}
             </style>
